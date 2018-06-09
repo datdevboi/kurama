a2757338f42bb7b6b361725cc39f09fbdf9f41e8
camilo17 <cami172013@gmail.com>
2018-06-09 18:08:32 -0500

    Created a simple test for Button

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,13 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import Button from './Button';
+
+
+const wrapper = shallow(<Button>Test</Button>);
+
+
+describe('Button Component', () => {
+    it('renders as expected', () => {
+        expect(wrapper).toBeTruthy();
+    })
+})
