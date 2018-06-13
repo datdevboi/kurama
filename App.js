d405153fcf50d1d6427a92f4c993e24ca4551a11
camilo17 <cami172013@gmail.com>
2018-06-13 15:20:22 -0500

    changed our main app to typescript

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import StoryUI from './storybook';
-
-
-
-export default StoryUI;
---
+++ App.ts
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import StoryUI from "./storybook";
+
+export default StoryUI;
