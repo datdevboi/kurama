44079b28a84c328688ef780c77971015aed2d379
camilo17 <cami172013@gmail.com>
2018-07-01 19:40:02 -0500

    Moved files to tsx and removed some unnecessary imports

--- App.ts
+++ App.ts
@@ -1,5 +1,3 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import StoryUI from "./storybook";
 
 export default StoryUI;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { TouchableHighlight } from "react-native";
+
+export interface IProps {
+  children: React.ReactNode;
+  /** prop1 description */
+  onPress: () => null;
+  /** prop2 description */
+  title: string;
+}
+
+export const Button = (props: IProps) => (
+  <TouchableHighlight onPress={props.onPress}>
+    {props.children}
+  </TouchableHighlight>
+);
+
+// Button.propTypes = {
+//   children: PropTypes.node.isRequired,
+//   onPress: PropTypes.func
+// };
+// Button.defaultProps = {
+//   onPress: () => {}
+// };
