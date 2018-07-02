5a62d71c8bbc0c7dc7920d04ca2734dd2857a88a
camilo17 <cami172013@gmail.com>
2018-07-01 19:41:15 -0500

    removed some comments and cleaned up the code

--- scripts/generateComponentData.ts
+++ scripts/generateComponentData.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import chalk from "chalk";
-// import chokidar from "chokidar";
+import chokidar from "chokidar";
 import * as reactdcts from "react-docgen-typescript";
 import util from "util";
 
@@ -11,29 +11,6 @@ const paths = {
   components: path.join(__dirname, "../src", "components"),
   output: path.join(__dirname, "../config", "componentData.ts")
 };
-
-// const enableWatchMode = process.argv.slice(2) == '--watch';
-// if (enableWatchMode) {
-//   // Regenerate component metadata when components or examples change.
-//   chokidar.watch([paths.examples, paths.components]).on('change', function(event, path) {
-//     generate(paths);
-//   });
-// } else {
-//   // Generate component metadata
-//   generate(paths);
-// }
-
-// function generate(paths) {
-//   var errors = [];
-//   var componentData = getDirectories(paths.components).map(function(componentName) {
-//     try {
-//       return getComponentData(paths, componentName);
-//     } catch(error) {
-//       errors.push('An error occurred while attempting to generate metadata for ' + componentName + '. ' + error);
-//     }
-//   });
-//   writeFile(paths.output, "module.exports = /* eslint-disable */ " + JSON.stringify(errors.length ? errors : componentData));
-// }
 
 function getDirectories(filepath: string) {
   return fs.readdirSync(filepath).filter(file => {
@@ -45,14 +22,6 @@ function readFile(filePath: string) {
   return fs.readFileSync(filePath, "utf-8");
 }
 
-const componentData = getDirectories(paths.components).map(fileName => {
-  const data = parse(
-    path.join(__dirname, `../src/components/${fileName}/${fileName}.tsx`)
-  );
-
-  return data[0];
-});
-
 function writeFile(filepath: string, content: any) {
   fs.writeFile(filepath, content, err => {
     err
@@ -61,7 +30,33 @@ function writeFile(filepath: string, content: any) {
   });
 }
 
-writeFile(
-  paths.output,
-  "export const componentData = " + util.inspect(componentData, { depth: null })
-);
+function generate(paths: any) {
+  const componentData = getDirectories(paths.components).map(fileName => {
+    const data = parse(
+      path.join(__dirname, `../src/components/${fileName}/${fileName}.tsx`)
+    );
+
+    return data[0];
+  });
+
+  writeFile(
+    paths.output,
+    "export const componentData = " +
+      util.inspect(componentData, { depth: null })
+  );
+}
+
+// const enableWatchMode = (process.argv.slice(2) as any === "--watch");
+// if (enableWatchMode) {
+//   // Regenerate component metadata when components or examples change.
+//   chokidar
+//     .watch(paths.components])
+//     .on("change", (event: any, pathss: string) => {
+//       generate(pathss);
+//     });
+// } else {
+//   // Generate component metadata
+//   generate(paths);
+// }
+
+generate(paths);
