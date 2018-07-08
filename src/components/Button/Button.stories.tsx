8a9cfc49b531e88292582b96cbe57d193c1d62f9
camilo17 <cami172013@gmail.com>
2018-07-08 17:28:51 -0500

    fixed an import

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,10 +3,10 @@ import { Text } from "react-native";
 import { storiesOf } from "@storybook/react-native";
 import { action } from "@storybook/addon-actions";
 import { Button } from "./Button";
-import CenterView from "../../utils/CenterView";
+// import CenterView from "../../utils/CenterView";
 
 storiesOf("Button", module)
-  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  // .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
   .add("with text", () => (
     <Button onPress={action("clicked-text")}>
       <Text>Hello Buttons</Text>
