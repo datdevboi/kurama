94fcb1c3f454adcbc9959610cb866b37556cfa9f
camilo17 <cami172013@gmail.com>
2018-07-08 20:08:41 -0500

    created a DrawerNavigator

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { createDrawerNavigator, DrawerItems } from "react-navigation";
+import { Container, Content, Icon, Header, Body, Title } from "native-base";
+import { Home } from "./screens/Home";
+
+const CustomDrawerContentComponent = (props: any) => (
+  <Container>
+    <Header style={styles.drawerHeader}>
+      <Body>
+        <Title>Kurama-UI</Title>
+      </Body>
+    </Header>
+    <Content>
+      <DrawerItems {...props} />
+    </Content>
+  </Container>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  drawerHeader: {
+    height: 100,
+    backgroundColor: "#9975B9"
+  }
+});
+
+export const App = createDrawerNavigator(
+  {
+    Home: {
+      screen: Home
+    }
+  },
+  {
+    initialRouteName: "Home",
+    drawerPosition: "left",
+    contentComponent: CustomDrawerContentComponent
+  }
+);
