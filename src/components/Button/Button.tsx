bbe22d676b36440cb82e0fe11e155d796dc251dc
camilo17 <cami172013@gmail.com>
2018-07-12 15:52:19 -0500

    Made our Button more customizable

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,68 @@
 import * as React from "react";
 
-import { TouchableHighlight } from "react-native";
+import { TouchableHighlight, Platform, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { render } from "enzyme";
+
+const platform = Platform.OS;
+
+const btnTypes = {
+  Primary: platform === "ios" ? "#007aff" : "#3F51B5",
+  Info: "#62B1F6",
+  Success: "#5cb85c",
+  Danger: "#d9534f",
+  Warning: "#f0ad4e",
+  Dark: "#000",
+  Light: "#f4f4f4"
+};
+
+const btnSizes = {
+  sm: 75
+};
 
 export interface IProps {
   children: React.ReactElement<any>;
-  /** prop1 description */
+  /** function when Button is pressed */
   onPress: () => void;
-  /** prop2 description */
+  /** The type of Button */
+  type?:
+    | "Primary"
+    | "Info"
+    | "Success"
+    | "Danger"
+    | "Warning"
+    | "Dark"
+    | "Light";
+  /** Should button be rounded */
+  rounded?: boolean;
+  /** Should button be full, taking all of the space of its container */
+  full?: boolean;
+  /** the opacity of the button once pressed */
+  activeOpacity?: number;
 }
 
-export const Button = (props: IProps) => (
-  <TouchableHighlight onPress={props.onPress}>
-    {props.children}
-  </TouchableHighlight>
-);
+const Btn = styled.TouchableOpacity<IProps>`
+  background-color: ${props => btnTypes[props.type]};
+  display: flex;
+  padding-top: 15;
+  height: 150;
+  width: 150;
+
+  ${props =>
+    props.rounded &&
+    `
+    border-radius: 10;
+  `};
+
+  ${props =>
+    props.full &&
+    `
+    flex: 1;
+  `};
+`;
+
+export const Button = (props: IProps) => {
+  const { children, ...otherProps } = props;
+
+  return <Btn {...otherProps}>{props.children}</Btn>;
+};
