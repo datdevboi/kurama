deda1c7bfd55f748cc6ef535e3614ce23af84375
camilo17 <cami172013@gmail.com>
2018-07-16 20:23:49 -0500

    Made tons of changes removed rounded prop and made a new styled component for our android devices

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,6 @@
 import * as React from "react";
 
-import {
-  TouchableHighlight,
-  Platform,
-  TouchableOpacity,
-  Text,
-  View
-} from "react-native";
+import { Platform, View, TouchableNativeFeedback } from "react-native";
 import styled from "styled-components/native";
 import { btnStyles } from "./btnStyles";
 
@@ -24,8 +18,7 @@ export interface IProps {
     | "Warning"
     | "Dark"
     | "Light";
-  /** Should button be rounded */
-  rounded?: boolean;
+
   /** Should button be full, taking all of the space of its container */
   full?: boolean;
   /** the opacity of the button once pressed */
@@ -35,14 +28,11 @@ export interface IProps {
   size?: "small" | "medium" | "large";
 }
 
+// IOS Button
 const Btn = styled.TouchableOpacity<IProps>`
   background-color: ${props => btnTypes[props.type || "Primary"]};
 
-  ${props =>
-    props.rounded &&
-    `
-    border-radius: 10;
-  `};
+  border-radius: 4;
 
   ${props =>
     props.full &&
@@ -58,6 +48,26 @@ const Btn = styled.TouchableOpacity<IProps>`
   `};
 `;
 
+// Android Button
+const TNFBtn = styled.View<IProps>`
+  background-color: ${props => btnTypes[props.type || "Primary"]};
+
+  border-radius: 4;
+
+  ${props =>
+    props.full &&
+    `
+  flex: 1;
+`};
+
+  ${props =>
+    props.size &&
+    `
+  height: ${btnSizes[props.size].height};
+  width: ${btnSizes[props.size].width}
+`};
+`;
+
 export class Button extends React.Component<IProps> {
   public static defaultProps: IProps = {
     activeOpacity: 0.5,
@@ -65,11 +75,11 @@ export class Button extends React.Component<IProps> {
     size: "medium"
   };
   public render() {
-    const { children, ...otherProps } = this.props;
+    const { children, size, type, onPress, full } = this.props;
 
     if (Platform.OS === "ios") {
       return (
-        <Btn {...otherProps}>
+        <Btn size={size} type={type} onPress={onPress} full={full}>
           <View
             style={{
               flex: 1,
@@ -78,10 +88,21 @@ export class Button extends React.Component<IProps> {
               flexDirection: "row"
             }}
           >
-            {this.props.children}
+            {children}
           </View>
         </Btn>
       );
     }
+
+    return (
+      <TouchableNativeFeedback
+        onPress={onPress}
+        background={TouchableNativeFeedback.Ripple("lightblue")}
+      >
+        <View style={{ backgroundColor: "blue", height: 150, width: 150 }}>
+          {children}
+        </View>
+      </TouchableNativeFeedback>
+    );
   }
 }
