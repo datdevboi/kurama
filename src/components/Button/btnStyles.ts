e248597acf59f47b2a6ee7bbedbcd0ea3261b665
camilo17 <cami172013@gmail.com>
2018-07-18 20:09:51 -0500

    added better typing to our btnStyles and exported btnTypes and btnSizes

--- src/components/Button/btnStyles.ts
+++ src/components/Button/btnStyles.ts
@@ -1,29 +1,48 @@
 import { Platform } from "react-native";
+import { lighten } from "polished";
 
 const platform = Platform.OS;
 
-export const btnStyles = {
-  btnTypes: {
-    Primary: platform === "ios" ? "#007aff" : "#3F51B5",
-    Info: "#62B1F6",
-    Success: "#5cb85c",
-    Danger: "#d9534f",
-    Warning: "#f0ad4e",
-    Dark: "#000",
-    Light: "#f4f4f4"
+export const lightenUp = lighten(0.2);
+
+interface IBtnTypes {
+  [key: string]: {
+    regular: string;
+    light: string;
+  };
+}
+
+interface IBtnSizes {
+  [key: string]: {
+    height: number;
+    width: number;
+  };
+}
+
+export const btnSizes: IBtnSizes = {
+  small: {
+    height: 0,
+    width: 0
   },
-  btnSizes: {
-    small: {
-      height: 0,
-      width: 0
-    },
-    medium: {
-      height: 50,
-      width: 100
-    },
-    large: {
-      height: 0,
-      width: 0
-    }
+  medium: {
+    height: 50,
+    width: 100
+  },
+  large: {
+    height: 0,
+    width: 0
   }
+};
+
+export const btnTypes: IBtnTypes = {
+  Primary:
+    platform === "ios"
+      ? { regular: "#007aff", light: lightenUp("#007aff") }
+      : { regular: "#3F51B5", light: lightenUp("#3F51B5") },
+  Info: { regular: "#62B1F6", light: lightenUp("#62B1F6") },
+  Success: { regular: "#5cb85c", light: lightenUp("#5cb85c") },
+  Danger: { regular: "#d9534f", light: lightenUp("#d9534f") },
+  Warning: { regular: "#f0ad4e", light: lightenUp("#f0ad4e") },
+  Dark: { regular: "#000", light: lightenUp("#000") },
+  Light: { regular: "#f4f4f4", light: lightenUp("#f4f4f4") }
 };
