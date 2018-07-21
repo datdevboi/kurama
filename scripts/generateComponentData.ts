730d23eff6d83558cd2d4c47c368247160400ba5
camilo17 <cami172013@gmail.com>
2018-07-21 18:18:41 -0500

    made our generateComponentData script include the examples for each component

--- scripts/generateComponentData.ts
+++ scripts/generateComponentData.ts
@@ -8,6 +8,7 @@ const parse = reactdcts.parse;
 
 const paths = {
   components: path.join(__dirname, "../src", "components"),
+  examples: path.join(__dirname, "../src", "examples"),
   output: path.join(__dirname, "../config", "componentData.ts")
 };
 
@@ -17,11 +18,7 @@ function getDirectories(filepath: string) {
   });
 }
 
-function readFile(filePath: string) {
-  return fs.readFileSync(filePath, "utf-8");
-}
-
-function writeFile(filepath: string, content: any) {
+export function writeFile(filepath: string, content: any) {
   fs.writeFile(filepath, content, err => {
     err
       ? // tslint:disable-next-line:no-console
@@ -30,15 +27,55 @@ function writeFile(filepath: string, content: any) {
         console.log(chalk.green("Component data saved."));
   });
 }
-
 function generate(paths: any) {
   const componentData = getDirectories(paths.components).map(fileName => {
     const data = parse(
       path.join(__dirname, `../src/components/${fileName}/${fileName}.tsx`)
     );
 
-    return data[0];
+    return {
+      name: fileName,
+      data: data[0],
+      examples: getExampleData(paths.examples, fileName)
+    };
   });
+
+  function readFile(filePath: string) {
+    return fs.readFileSync(filePath, "utf-8");
+  }
+  function getFiles(filepath: string) {
+    return fs.readdirSync(filepath).filter(file => {
+      return fs.statSync(path.join(filepath, file)).isFile();
+    });
+  }
+
+  function getExampleFiles(examplesPath: any, componentName: any) {
+    let exampleFiles: any = [];
+    try {
+      exampleFiles = getFiles(path.join(examplesPath, componentName));
+    } catch (error) {
+      console.log(chalk.red(`No examples found for ${componentName}.`));
+    }
+    return exampleFiles;
+  }
+
+  function getExampleData(examplesPath: string, componentName: string) {
+    const examples = getExampleFiles(examplesPath, componentName);
+    return examples.map((file: string) => {
+      const filePath = path.join(examplesPath, componentName, file);
+      const content = readFile(filePath);
+      const info = parse(filePath);
+      return {
+        // // By convention, component name should match the filename.
+        // // So remove the .js extension to get the component name.
+        // name: file.slice(0, -3),
+        // description: info.description,
+        name: info[0].displayName,
+        filePath,
+        code: content
+      };
+    });
+  }
 
   writeFile(
     paths.output,
