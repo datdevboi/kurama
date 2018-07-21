d4483ae199689bda16a7378de21f39c4315dcaff
camilo17 <cami172013@gmail.com>
2018-07-20 20:06:20 -0500

    removed destructuring Component class from react and made more examples of our button

--- src/screens/ButtonExample.tsx
+++ src/screens/ButtonExample.tsx
@@ -3,9 +3,7 @@ import { Text, View } from "react-native";
 import CustomHeader from "../CustomHeader";
 import { Button } from "../components/Button/Button";
 
-const { Component } = React;
-
-export class ButtonExample extends Component<any> {
+export class ButtonExample extends React.Component<any> {
   public render() {
     return (
       <View style={{ flex: 1 }}>
@@ -13,8 +11,26 @@ export class ButtonExample extends Component<any> {
           drawerOpen={() => this.props.navigation.openDrawer()}
           title={"Button"}
         />
-        <Button onPress={() => console.log("pressed")}>
-          <Text style={{ color: "white" }}>Hello</Text>
+        <Button type="Primary" onPress={() => console.log("pressed")}>
+          <Text style={{ color: "white" }}>Primary</Text>
+        </Button>
+
+        <Button type="Info" onPress={() => console.log("pressed")}>
+          <Text style={{ color: "white" }}>Info</Text>
+        </Button>
+
+        <Button type="Success" onPress={() => console.log("pressed")}>
+          <Text style={{ color: "white" }}>Success</Text>
+        </Button>
+
+        <Button type="Danger" onPress={() => console.log("pressed")}>
+          <Text style={{ color: "white" }}>Danger</Text>
+        </Button>
+        <Button type="Dark" onPress={() => console.log("pressed")}>
+          <Text style={{ color: "white" }}>Dark</Text>
+        </Button>
+        <Button type="Warning" onPress={() => console.log("pressed")}>
+          <Text style={{ color: "white" }}>Warning</Text>
         </Button>
       </View>
     );
