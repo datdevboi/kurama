ad9ee8fe0d7cdf8f2efeda5c5a444c18c9d146ae
camilo17 <cami172013@gmail.com>
2018-07-21 11:50:22 -0500

    fixed our Header styles for android

--- src/CustomHeader.tsx
+++ src/CustomHeader.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform, StatusBar } from "react-native";
 
 import { Header, Body, Title, Content, Left, Icon, Right } from "native-base";
 
 class CustomHeader extends React.Component<any> {
   public render() {
     return (
-      <Header style={{ backgroundColor: "#9975B9" }}>
+      <Header style={styles.header}>
         <Left>
           <Icon name="ios-menu" onPress={() => this.props.drawerOpen()} />
         </Left>
@@ -18,4 +18,16 @@ class CustomHeader extends React.Component<any> {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#9975B9",
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight
+      }
+    })
+  }
+});
+
 export default CustomHeader;
